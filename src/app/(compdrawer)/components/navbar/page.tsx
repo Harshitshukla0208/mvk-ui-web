8da120f92d10b6416e1813de01c22f7997264ec1
Harshit Shukla <mrharshit2409@gmail.com>
2024-06-09 16:10:57 +0530

    added navbar components page

---
+++ src/app/(compdrawer)/components/navbar/page.tsx
@@ -0,0 +1,52 @@
+"use client"
+import React from 'react';
+import { Navbar1 } from 'mvk-ui';
+import NavbarCodes from '@/components/CodeBoxes/NavbarCodes';
+
+const Page = () => {
+
+    const navbarLinks = [
+        { label: 'Home', url: '/' },
+        { label: 'About', url: '/about' },
+        { label: 'Services', url: '/services' },
+        { label: 'Contact', url: '/contact' }
+    ];
+
+    return (
+        <div className='mt-16 w-full px-4'>
+            <h1 className='font-bold text-3xl'>Navbar</h1>
+            <div className='flex gap-2 mt-5'>
+                <p className='opacity-40 hover:opacity-60'>#</p>
+                <p className='component-preview-title'>Navbar is used to show a navigation bar on the top of the page.</p>
+            </div>
+            <div className='flex gap-2 mt-5'>
+                <p className='opacity-40 hover:opacity-60'>#</p>
+                <p className='component-preview-title'>Preview</p>
+            </div>
+            <div className="relative mt-5 border-[#f0f0f0] border-[2px] rounded-xl w-full h-80 p-4 bg-white overflow-hidden mx-auto">
+                <div className="absolute inset-0 rotated-bg transform origin-top-left" style={{ width: '120%', height: '205%' }}>
+                    {/* This div is only for background */}
+                </div>
+                <div className="relative flex justify-center items-center">
+                    <div className='flex flex-col md:flex-row gap-3 justify-center w-full'>
+                        <Navbar1 
+                            brand="My Brand" 
+                            links={navbarLinks} 
+                            backgroundColor="#fff" 
+                            textColor="#333"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className='flex gap-2 mt-5'>
+                <p className='opacity-40 hover:opacity-60'>#</p>
+                <p className='component-preview-title'>Code</p>
+            </div>
+            <div>
+                <NavbarCodes />
+            </div>
+        </div>
+    )
+}
+
+export default Page;
---
+++ src/components/CodeBoxes/NavbarCodes.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import Image from 'next/image';
+import clipboardIcon from '../../assets/content_copy_24dp_FILL0_wght400_GRAD0_opsz24.svg';
+import success from '../../assets/check_24dp_FILL0_wght400_GRAD0_opsz24.svg';
+
+const NavbarCodes = () => {
+    const codeString1 =
+    `
+    "use client"  // for nextJS
+    import { Navbar1 } from 'mvk-ui';
+
+    const App = () => {
+        const navbarLinks = [
+            { label: 'Home', url: '/' },
+            { label: 'About', url: '/about' },
+            { label: 'Services', url: '/services' },
+            { label: 'Contact', url: '/contact' }
+        ];
+
+        return (
+            <div className="app-container">
+            <Navbar1 
+                brand="My Brand" 
+                links={navbarLinks} 
+                backgroundColor="#fff" 
+                textColor="#333"
+            />
+            {/* Other components */}
+            </div>
+        );
+    };
+
+    export default App;
+    `;
+
+    const [copy1, setCopy1] = useState(false);
+
+    return (
+        <div className="p-4 flex justify-center">
+            <div className="relative w-[100%] md:w-[500px] bg-[#282c34] rounded-xl overflow-hidden">
+                <SyntaxHighlighter language="jsx" style={atomOneDark} customStyle={{ margin: 0 }}>
+                    {codeString1}
+                </SyntaxHighlighter>
+                <div className="absolute top-2 right-2">
+                    {copy1 ? (
+                        <button className="py-1 flex items-center text-white">
+                            <Image src={success} alt="Code Copied" />
+                            <p className="ml-2 text-xs">Copied!</p>
+                        </button>
+                    ) : (
+                        <button
+                            className="py-1 flex items-center text-white"
+                            onClick={() => {
+                                navigator.clipboard.writeText(codeString1);
+                                setCopy1(true);
+                                setTimeout(() => {
+                                    setCopy1(false);
+                                }, 3000);
+                            }}
+                        >
+                            <Image src={clipboardIcon} alt="Copy Code" />
+                            <p className="ml-2 text-xs">Copy code</p>
+                        </button>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NavbarCodes;
