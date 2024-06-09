818475cf8b3e0489d9e877d272906e4ed2e95909
Harshit Shukla <mrharshit2409@gmail.com>
2024-06-09 16:53:53 +0530

    added input components page

---
+++ src/app/(compdrawer)/components/input/page.tsx
@@ -0,0 +1,41 @@
+"use client"
+import React from 'react'
+import { Input1 } from 'mvk-ui'
+import InputBoxesCodes from '@/components/CodeBoxes/InputBoxesCodes'
+
+const Page = () => {
+    return (
+        <div className='mt-16 w-full px-4'>
+            <h1 className='font-bold text-3xl'>Inputs</h1>
+            <div className='flex gap-2 mt-5'>
+                <p className='opacity-40 hover:opacity-60'>#</p>
+                <p className='component-preview-title'>Input boxes: the blank spaces where you type stuff.</p>
+            </div>
+            <div className='flex gap-2 mt-5'>
+                <p className='opacity-40 hover:opacity-60'>#</p>
+                <p className='component-preview-title'>Preview</p>
+            </div>
+            <div className="relative mt-5 border-[#f0f0f0] border-[2px] rounded-xl h-64 md:h-[12rem] w-full md:w-[75%] p-4 bg-white overflow-hidden mx-auto">
+                <div className="absolute inset-0 rotated-bg transform origin-top-left" style={{ width: '120%', height: '205%' }}>
+                    {/* This div is only for background */}
+                </div>
+                <div className="relative flex justify-center items-center h-full">
+                    <div className='flex flex-col md:flex-row gap-3 justify-center'>
+                        <Input1 label="Username" placeholder="Username..."/>
+                        <Input1 label="Password" placeholder="Password..." type="password"/>
+                        <Input1 label="Email" placeholder="Email..." type="email"/>
+                    </div>
+                </div>
+            </div>
+            <div className='flex gap-2 mt-5'>
+                <p className='opacity-40 hover:opacity-60'>#</p>
+                <p className='component-preview-title'>Code</p>
+            </div>
+            <div>
+                <InputBoxesCodes />
+            </div>
+        </div>
+    )
+}
+
+export default Page
