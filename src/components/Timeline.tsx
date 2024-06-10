b7f99dedc98ce6af4fa55a968a25a406f05b266d
Harshit Shukla <mrharshit2409@gmail.com>
2024-06-10 15:17:10 +0530

    minor changes

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -29,99 +29,99 @@ export default function NoOppositeContent() {
 
     return (
         <Timeline
-        sx={{
-            [`& .${timelineItemClasses.root}:before`]: {
-            flex: 0,
-            padding: 0,
-            },
-        }}
-        className="ml-4 md:ml-[60px]"
+            sx={{
+                [`& .${timelineItemClasses.root}:before`]: {
+                    flex: 0,
+                    padding: 0,
+                },
+            }}
+            className="ml-4 md:ml-[60px]"
         >
-        <TimelineItem>
-            <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent className="text-lg">
-            Jump right into building with mvk-ui, install it via package manager.
-            </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-            <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent className="flex flex-col md:flex-row items-start md:items-center text-lg">
-            <p className='md:mt-[-40px]'>Install mvk-ui as a Node package</p>
-            <div className="flex justify-center mt-7 md:mt-[-8px] md:ml-10 mb-7 w-full md:w-[210px] bg-[#282c34] rounded-lg overflow-hidden">
-                <SyntaxHighlighter language="jsx" style={atomOneDark} customStyle={{ padding: '10px', paddingLeft: '20px' }}>
-                {codeString1}
-                </SyntaxHighlighter>
-                <div className="flex justify-between px-4 text-white text-xs items-center">
-                {copy1 ? (
-                    <button className="py-1 items-center">
-                    <span className="text-base mt-1 text-white">
-                        <Image src={success} alt="Code Copied" />
-                    </span>
-                    </button>
-                ) : (
-                    <button
-                    className="py-1 items-center"
-                    onClick={() => {
-                        navigator.clipboard.writeText(codeString1);
-                        setCopy1(true);
-                        setTimeout(() => {
-                        setCopy1(false);
-                        }, 3000);
-                    }}
-                    >
-                    <span className="text-base mt-1 text-white">
-                        <Image src={clipboardIcon} alt="Copy Code" />
-                    </span>
-                    </button>
-                )}
-                </div>
-            </div>
-            </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-            <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent className="text-lg">
-            Import the component you want to use.
-            </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-            <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent className="text-lg">
-            And just copy the code and paste it where you want to use it.
-            </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-            <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent className="text-lg">
-            Make sure to pass the suitable props along with the component.
-            </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-            <TimelineSeparator>
-            <TimelineDot />
-            </TimelineSeparator>
-            <TimelineContent className="flex flex-col md:flex-row items-start md:items-center text-lg">
-            <p className='md:mt-[-30px]'>Here's the demo of a component.</p>
-            <div className="mt-4 md:mt-[-30px] md:ml-10">
-                <Selector options={dropdownOptions} placeholder="Select an option" onSelect={handleSelect} />
-            </div>
-            </TimelineContent>
-        </TimelineItem>
+            <TimelineItem>
+                <TimelineSeparator>
+                    <TimelineDot />
+                    <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent className="text-lg">
+                    Jump right into building with mvk-ui, install it via package manager.
+                </TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+                <TimelineSeparator>
+                    <TimelineDot />
+                    <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent className="flex flex-col md:flex-row items-start md:items-center text-lg">
+                    <p className="md:mt-[-40px]">Install mvk-ui as a Node package</p>
+                    <div className="flex justify-center mt-7 md:mt-[-8px] md:ml-10 mb-7 w-full md:w-[210px] bg-[#282c34] rounded-lg overflow-hidden">
+                        <SyntaxHighlighter language="jsx" style={atomOneDark} customStyle={{ padding: '10px', paddingLeft: '20px' }}>
+                            {codeString1}
+                        </SyntaxHighlighter>
+                        <div className="flex justify-between px-4 text-white text-xs items-center">
+                            {copy1 ? (
+                                <button className="py-1 items-center">
+                                    <span className="text-base mt-1 text-white">
+                                        <Image src={success} alt="Code Copied" />
+                                    </span>
+                                </button>
+                            ) : (
+                                <button
+                                    className="py-1 items-center"
+                                    onClick={() => {
+                                        navigator.clipboard.writeText(codeString1);
+                                        setCopy1(true);
+                                        setTimeout(() => {
+                                            setCopy1(false);
+                                        }, 3000);
+                                    }}
+                                >
+                                    <span className="text-base mt-1 text-white">
+                                        <Image src={clipboardIcon} alt="Copy Code" />
+                                    </span>
+                                </button>
+                            )}
+                        </div>
+                    </div>
+                </TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+                <TimelineSeparator>
+                    <TimelineDot />
+                    <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent className="text-lg">
+                    Import the component you want to use.
+                </TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+                <TimelineSeparator>
+                    <TimelineDot />
+                    <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent className="text-lg">
+                    And just copy the code and paste it where you want to use it.
+                </TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+                <TimelineSeparator>
+                    <TimelineDot />
+                    <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent className="text-lg">
+                    Make sure to pass the suitable props along with the component.
+                </TimelineContent>
+            </TimelineItem>
+            <TimelineItem>
+                <TimelineSeparator>
+                    <TimelineDot />
+                </TimelineSeparator>
+                <TimelineContent className="flex flex-col md:flex-row items-start md:items-center text-lg">
+                    <p className="md:mt-[-30px]">Here&apos;s the demo of a component.</p>
+                    <div className="mt-4 md:mt-[-30px] md:ml-10">
+                        <Selector options={dropdownOptions} placeholder="Select an option" onSelect={handleSelect} />
+                    </div>
+                </TimelineContent>
+            </TimelineItem>
         </Timeline>
     );
 }
